280c9f89797299ed22550b58223a05e15d356862
tudormardare <tudormardare@stud.unifi.it>
2023-03-02 13:12:47 +0100

    Aggiunta classe Entity

---
+++ cmake-build-debug/CMakeFiles/Jumper.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/tudor/OneDrive - stud.unifi.it/Documenti/CLionProjects/Jumper/Entity.cpp" "CMakeFiles/Jumper.dir/Entity.cpp.obj" "gcc" "CMakeFiles/Jumper.dir/Entity.cpp.obj.d"
+  "C:/Users/tudor/OneDrive - stud.unifi.it/Documenti/CLionProjects/Jumper/main.cpp" "CMakeFiles/Jumper.dir/main.cpp.obj" "gcc" "CMakeFiles/Jumper.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Jumper.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Jumper.dir/Entity.cpp.obj"
+  "CMakeFiles/Jumper.dir/Entity.cpp.obj.d"
+  "CMakeFiles/Jumper.dir/main.cpp.obj"
+  "CMakeFiles/Jumper.dir/main.cpp.obj.d"
+  "Jumper.exe"
+  "Jumper.exe.manifest"
+  "Jumper.pdb"
+  "libJumper.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Jumper.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
